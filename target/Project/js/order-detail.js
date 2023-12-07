4291b802ab1f8b365abfcdf88b8dd24320b2f605
Lê Bảo Đặng <113536622+Le-Bao-Dang@users.noreply.github.com>
2023-12-07 10:57:13 +0700

    dang

--- target/Project/js/order-detail.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $('#infor-tab').click(function () {
-        window.location = "http://localhost:8080/userProfile";
-    });
-});
--- target/Project/js/user-manager.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.edit-user').click(function () {
-        let idUer = $(this).val().trim();
-        let varieties = $('.user-varieties' + $(this).val()).val().trim();
-        let stt = $('.user-stt' + $(this).val()).val().trim();
-        window.location = "/editUserManager?id=" + idUer + "&varieties=" + varieties+ "&stt=" + stt;
-    })
-})
